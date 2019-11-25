fde8ae1d63cfb7495e0e16d4bd07826c3d86f74f
dumin199101 <1766266374@qq.com>
2019-11-25 15:35:43 +0800

    Flask教程-02 Flask-配置文件

--- 15 Flask/page1.py
+++ 15 Flask/page1.py
@@ -5,12 +5,29 @@
 # 开发工具：PyCharm
 
 # 第一个Flask小程序
-from flask import Flask
+from flask import Flask,current_app
 
 # __name__代表当前模块的名字，flask以这个模块所在的目录为总目录，以这个目录中的static为静态目录，templates为模板目录
 # 当前启动文件：__name__就是__main__,否则就是模块名字
 # Flask初始化参数：static_url_path,static_folder,template_folder
 app = Flask(__name__,static_url_path="/python",static_folder="static",template_folder="templates")
+
+# 加载配置文件
+# 1.使用配置文件方式
+app.config.from_pyfile("config.cfg")
+
+# 2.使用对象方式
+# class Config(object):
+#     DEBUG = True
+# app.config.from_object(Config)
+
+
+#3.使用字典方式
+# app.config["DEBUG"] = True
+
+# 读取配置
+print(app.config.get("HOST"))
+
 
 @app.route("/")
 def index():
@@ -18,7 +35,9 @@ def index():
     定义视图函数
     :return:
     """
-    return "Hello Flask"
+    # a = 1 / 0
+    print(current_app.config.get("HOST"))
+    return "Hello Python Flask!!"
 
 if __name__ == '__main__':
-     app.run()
+     app.run(port=5002)
