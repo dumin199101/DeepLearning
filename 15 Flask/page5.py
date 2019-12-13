a43219ab9d37f17dccbdf7990d513775842defd6
dumin199101 <1766266374@qq.com>
2019-12-13 15:25:46 +0800

    Flask教程-05 Flask-abort函数,自定义响应信息，错误处理，json数据返回

--- 15 Flask/page5.py
+++ 15 Flask/page5.py
@@ -4,7 +4,7 @@
 # 文件名称：abort
 # 开发工具：PyCharm
 # abort函数
-from flask import Flask,request,abort,Response
+from flask import Flask,request,abort,Response,make_response,jsonify
 
 app = Flask(__name__,static_url_path="/static",static_folder="static",template_folder="templates")
 
@@ -14,7 +14,13 @@ def index():
     定义视图函数
     :return:
     """
-    return "Hello Python Flask!!"
+    # 响应信息，状态码，响应头
+    # return 'index page',200,[("name","ok"),("age","22")]
+    resp = make_response("index page 2")
+    resp.headers['city'] = 'bz'
+    resp.status = '666 custom code'
+    return resp
+
 
 @app.route("/login",methods=['POST','GET'])
 def login():
@@ -22,11 +28,23 @@ def login():
     pwd = request.form.get('pwd','')
     if name!='zhangsan' or pwd!='123456':
         # 返回标准状态码
-        # abort(404)
+        abort(404)
         # 返回响应信息
         resp = Response('login failed')
         abort(resp)
 
+@app.route("/json")
+def json():
+    data = {
+        "code":1,
+        "city":'beijing'
+    }
+    return jsonify(data)
+
+# 自定义错误处理
+@app.errorhandler(404)
+def error(error):
+    return 'get Error:%s' % error
 
 if __name__ == '__main__':
     # 查看url路由
